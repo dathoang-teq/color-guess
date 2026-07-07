ec1f3e596f8c3f51fa1103b9618e1b711d5ec133
dathoang-teq <dathoang@teqnological.asia>
2026-07-07 15:49:00 +0700

    chore: update service worker

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,49 @@
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  '/index.html',
+  '/quizz'
+];
+
+self.addEventListener('install', function(event) {
+	self.skipWaiting();
+	event.waitUntil(
+		caches.open(CACHE_NAME)
+		.then(function(cache) {
+			return cache.addAll(urlsToCache);
+		})
+	);
+});
+
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+		caches.match(event.request)
+		.then(function(response) {
+			if (response) {
+				return response;
+			}
+			return fetch(event.request).then(function(response) {
+				if (langUrlRe.exec(event.request.url)) {
+					caches.open(CACHE_NAME).then(function(cache) {
+						cache.put(langUrlRe.exec(event.request.url)[1], response);
+					});
+				}
+				return response.clone();
+			});
+		})
+	);
+});
+
+self.addEventListener('activate', function(event) {
+	event.waitUntil(
+		caches.keys().then(function(cacheNames) {
+			return Promise.all(
+				cacheNames.map(function(cacheName) {
+					if (cacheName != CACHE_NAME) {
+						return caches.delete(cacheName);
+					}
+				})
+			);
+		})
+	);
 });
